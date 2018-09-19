b9faf1dbfb3f95a986a0989970a4a1c27b17c248
Colin Rupp <colin@thinkful.com>
2018-09-18 17:54:57 -0700

    added CRUD operations to router/notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -1,50 +1,95 @@
 'use strict';
 
 const express = require('express');
-
+const Note = require('../models/note.js');
 const router = express.Router();
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  let searchTerm = req.query.searchTerm;
+  let filter = {};
+  if (searchTerm) {
+    const expr = RegExp(searchTerm, 'gi');
+    filter = {$or: [{'title': expr}, {'content': expr}]};
+  }
 
-  console.log('Get All Notes');
-  res.json([
-    { id: 1, title: 'Temp 1' },
-    { id: 2, title: 'Temp 2' },
-    { id: 3, title: 'Temp 3' }
-  ]);
-
+  Note.find(filter)
+    .sort({ updatedAt: 'desc' })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
 router.get('/:id', (req, res, next) => {
 
-  console.log('Get a Note');
-  res.json({ id: 1, title: 'Temp 1' });
-
+  let id = req.params.id;
+  Note.findById(id)
+    .then((results) => {
+      res.json(results);
+    })
+    .catch(err => next(err));
 });
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
+  const { title, content } = req.body;
 
-  console.log('Create a Note');
-  res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
+  if(!title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = {
+    title: title,
+    content: content,
+  };
+
+  Note.create(newItem)
+    .then( result => {
+      res.location(`${req.originalUrl}/${result.id}`).json(result);
+    })
+    .catch(err => next(err));
+  //console.log('Create a Note');
+  //res.location('path/to/new/document').status(201).json({ id: 2, title: 'Temp 2' });
 
 });
 
 /* ========== PUT/UPDATE A SINGLE ITEM ========== */
 router.put('/:id', (req, res, next) => {
+  if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = `Request path id ${req.params.id} and ${req.body.id} must match`;
+    console.error(message);
+  }
 
-  console.log('Update a Note');
-  res.json({ id: 1, title: 'Updated Temp 1' });
+  const id = req.params.id;
+  const toUpdate = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Note.findByIdAndUpdate(id, toUpdate, {new: true})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => next(err));
 
 });
 
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:id', (req, res, next) => {
-
-  console.log('Delete a Note');
-  res.status(204).end();
+  const id = req.params.id;
+  Note.findByIdAndRemove(id)
+    .then(() => {
+      res.sendStatus(204).end();
+    })
+    .catch(err => next(err));
 });
 
 module.exports = router;
---
+++ scratch/queries.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/note');
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchTerm = 'aaaaaarrrrrrr';
+    let filter = {};
+
+    if (searchTerm) {
+      const expr = RegExp(searchTerm, 'gi');
+       filter = {$or: [{'title': expr}, {'content': expr}]};
+     }
+     return Note.find(filter).sort({ updatedAt: 'desc' });
+   })
+    .then(results => {
+
+     console.log(results);
+   })
+   .then(() => {
+     return mongoose.disconnect()
+   })
+   .catch(err => {
+     console.error(`ERROR: ${err.message}`);
+     console.error(err);
+   });
+
+
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const foundNote = Note.findById('000000000000000000000005');
+//     return foundNote;
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect();
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`); /* eslint-disable-line no-console */
+//     console.error(err); /* eslint-disable-line no-console */
+//   });
+
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const updateObj = {title: 'How to add a new note', content: 'you hardwire it into your function, at least in this challenge...'};
+//     //this code is pointless here, since we are hardcoding our created object but will be useful in error checking
+//     if (!updateObj.title) {
+//       const message = `new object must contain a title`;
+//       console.error(message);  /* eslint-disable-line no-console */
+//
+//     }
+//     return Note.create(updateObj);
+//   })
+//   .then(results => {
+//     console.log(results); /* eslint-disable-line no-console */
+//
+//   })
+//   .catch(err => console.error(err));  /* eslint-disable-line no-console */
+
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const updateObj = {title: 'foooooooo', content: 'baaaaaaarrrrrrr', _id:'000000000000000000000002'};
+//     const id = '000000000000000000000002';
+//     //validation code.  useless in this challenge but will ned when transfering to actual noteful app
+//     if(!(id && updateObj.id && id === updateObj.id)) {
+//       const message = `Request path id ${id} and ${updateObj.id} must match`;
+//       console.error(message); /* eslint-disable-line no-console */
+//     }
+//     return Note.findByIdAndUpdate(id, updateObj, {new: true});
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(err => console.error(err));
+
+//  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//    .then(() => {
+//      const id = '000000000000000000000002';
+//      Note.findByIdAndRemove(id);
+//    })
+//    .then(() => console.log('deleted item with ID 000000000000000000000002')
+//    .catch(err => console.error(err))
+
+
