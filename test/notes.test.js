80483df3060b5fe0a3266be125d6c6007e350808
Colin Rupp <colin@thinkful.com>
2018-09-21 15:57:13 -0700

    added 400 and 404 tests for put

--- test/notes.test.js
+++ test/notes.test.js
@@ -1,11 +1,19 @@
+'use strict';
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 
 const app = require('../server.js');
 const { TEST_MONGODB_URI } = require('../config.js');
+
 const Note = require('../models/note.js');
+const Folder = require('../models/folder.js');
+const Tag = require('../models/tags.js');
+
 const seedNotes = require('../db/seed/notes.js');
+const folders = require('../db/seed/folders.js');
+const tags = require('../db/seed/folders.js');
 
 const expect = chai.expect;
 chai.use(chaiHttp);
@@ -18,7 +26,14 @@ describe('Notes API resource', function () {
   });
 
   beforeEach(function () {
-    return Note.insertMany(seedNotes);
+    return Promise.all([
+      Note.insertMany(seedNotes),
+      Folder.insertMany(folders),
+      Folder.createIndexes(),
+      Tag.insertMany(tags),
+      Tag.createIndexes()
+    ]);
+
   });
 
   afterEach(function () {
@@ -32,7 +47,8 @@ describe('Notes API resource', function () {
 
   /*=============== GET by ID Test =============== */
   describe('GET /api/notes/:id', function () {
-    it('should return correct notes', function () {
+
+    it('should return correct fields in notes', function () {
       let note;
       return Note.findOne()
         .then(foundNote => {
@@ -60,13 +76,6 @@ describe('Notes API resource', function () {
           expect(res).to.have.status(400);
         });
     });
-  //it('should respond with status 404 when ID does not exist', function () {
-  //return chai.request(app)
-  //  .get('/api/notes/stringof24c0')
-  //  .then(res => {
-  //    expect(res).to.have.status(404);
-  //  });
-  //});
   });
 
   /* ==================  GET all ============= */
@@ -83,13 +92,21 @@ describe('Notes API resource', function () {
           expect(res.body).to.have.length(data.length);
         });
     });
-    //it('should returns an empty array for invalid searchTerm ' function() {
+    //
+    // it('should return a list of notes where the notes have the correct fields' ...........)
+    //
+    // it('should return the correct search result for a searchTerm query')
+    //
+    // it('should return correct search results for a folderId query',)
+    //
+    //
 
 
   });
 
   /* =================  POST TESTS ================ */
   describe('POST /api/notes', function() {
+
     it('should create a new note and return a new item when given valid data', function () {
       const newNote = {
         'title': 'Testing a New Note',
@@ -117,6 +134,7 @@ describe('Notes API resource', function () {
           expect(new Date(res.body.createdAt)).to.eql(note.updatedAt);
         });
     });
+
     it('should return a 400 error if given a post without a title', function () {
       const newNote = {'content': 'content without a title'};
       let res;
@@ -139,7 +157,8 @@ describe('Notes API resource', function () {
       let res;
       let og_id;
 
-      return Note.findOne()
+      return Note
+        .findOne()
         .then(foundNote => {
           og_id = foundNote.id;
           return chai.request(app)
@@ -152,6 +171,8 @@ describe('Notes API resource', function () {
           expect(res).to.have.status(200);
           expect(res.body).to.be.an('object');
           expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt', 'folderId', 'tags');
+          expect(res.body.title).to.equal(updateNote.title);
+          expect(res.body.content).to.equal(updateNote.content);
           return Note.findById(res.body.id);
         })
         .then(dbNote => {
@@ -162,22 +183,43 @@ describe('Notes API resource', function () {
           expect(res.body.content).to.equal(dbNote.content);
           expect(dbNote.content).to.equal(updateNote.content);
           expect(new Date(res.body.updatedAt)).to.eql(dbNote.updatedAt);
+          expect(new Date(res.body.updatedAt)).to.eql(dbNote.updatedAt);
         });
     });
 
     it('status 404 when an item is updated with invalid data', function () {
 
-      const updateNote = {'content': 'content without a title'};
+      const updateNote = {'title': 'sdfsdfsdf', 'content': 'content without a title'};
       let res;
 
       return chai.request(app)
-        .put('/api/notes')
+        .put('/api/notes/BADPATH')
         .send(updateNote)
         .then(function (_res) {
           res = _res;
           expect(res).to.have.status(404);
-          expect(res).to.be.json;
         });
+    });
+
+    it('should return a status 400 and an error when `id` is not valid', function() {
+
+      const updateNote = {'content': 'good content' };
+
+      let res;
+      return Note.findOne()
+        .then(_res => {
+          res = _res;
+          return chai.request(app)
+            .put(`/api/notes/${res.id}`)
+            .send(updateNote);
+        })
+        .then(data => {
+          expect(data).to.have.status(400);
+          expect(data).to.be.json;
+          expect(data.body).to.be.an('object');
+          expect(data.body.message).to.equal('Missing `title` in request body');
+        });
+
     });
   });
 
